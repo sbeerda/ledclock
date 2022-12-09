769c7e75307b1117852f51a14a8caea9c0f4bb29
sbeerda <44503718+sbeerda@users.noreply.github.com>
2022-12-09 13:11:56 -0800

    Update um_ledclock.h
    change digit order 
    
    \Segment order: g, b, a, f, e, d, c

--- wled00/um_ledclock.h
+++ wled00/um_ledclock.h
@@ -323,44 +323,44 @@ public:
     void setup() {
         // digit 1
         dHoursT.setShowZero(!hideZero);
-        dHoursT.mapSegment(_7SEG_SEG_A, 6, 7);
-        dHoursT.mapSegment(_7SEG_SEG_B, 8, 9);
+        dHoursT.mapSegment(_7SEG_SEG_A, 4, 5);
+        dHoursT.mapSegment(_7SEG_SEG_B, 2, 3);
         dHoursT.mapSegment(_7SEG_SEG_C, 12, 13);
-        dHoursT.mapSegment(_7SEG_SEG_D, 1, 0);
-        dHoursT.mapSegment(_7SEG_SEG_E, 3, 2);
-        dHoursT.mapSegment(_7SEG_SEG_F, 5, 4);
-        dHoursT.mapSegment(_7SEG_SEG_G, 11, 10);
+        dHoursT.mapSegment(_7SEG_SEG_D, 10, 11);
+        dHoursT.mapSegment(_7SEG_SEG_E, 8, 9);
+        dHoursT.mapSegment(_7SEG_SEG_F, 6, 7);
+        dHoursT.mapSegment(_7SEG_SEG_G, 0, 1);
 
         // digit 2
-        dHoursO.mapSegment(_7SEG_SEG_A, 20, 21);
-        dHoursO.mapSegment(_7SEG_SEG_B, 22, 23);
+        dHoursO.mapSegment(_7SEG_SEG_A, 18, 19);
+        dHoursO.mapSegment(_7SEG_SEG_B, 16, 17);
         dHoursO.mapSegment(_7SEG_SEG_C, 26, 27);
-        dHoursO.mapSegment(_7SEG_SEG_D, 15, 14);
-        dHoursO.mapSegment(_7SEG_SEG_E, 17, 16);
-        dHoursO.mapSegment(_7SEG_SEG_F, 19, 18);
-        dHoursO.mapSegment(_7SEG_SEG_G, 25, 24);
+        dHoursO.mapSegment(_7SEG_SEG_D, 24, 25);
+        dHoursO.mapSegment(_7SEG_SEG_E, 22, 23);
+        dHoursO.mapSegment(_7SEG_SEG_F, 20, 21);
+        dHoursO.mapSegment(_7SEG_SEG_G, 14, 15);
 
         sep.map(2,
-            2, 0, 29,
-            4, 0, 28);
+            2, 0, 28,
+            4, 0, 29);
 
         // digit 3
-        dMinutesT.mapSegment(_7SEG_SEG_A, 36, 37);
-        dMinutesT.mapSegment(_7SEG_SEG_B, 38, 39);
+        dMinutesT.mapSegment(_7SEG_SEG_A, 34, 35);
+        dMinutesT.mapSegment(_7SEG_SEG_B, 32, 33);
         dMinutesT.mapSegment(_7SEG_SEG_C, 42, 43);
-        dMinutesT.mapSegment(_7SEG_SEG_D, 31, 30);
-        dMinutesT.mapSegment(_7SEG_SEG_E, 33, 32);
-        dMinutesT.mapSegment(_7SEG_SEG_F, 35, 34);
-        dMinutesT.mapSegment(_7SEG_SEG_G, 41, 40);
+        dMinutesT.mapSegment(_7SEG_SEG_D, 40, 41);
+        dMinutesT.mapSegment(_7SEG_SEG_E, 38, 39);
+        dMinutesT.mapSegment(_7SEG_SEG_F, 36, 37);
+        dMinutesT.mapSegment(_7SEG_SEG_G, 30, 31);
 
         // digit 4
-        dMinutesO.mapSegment(_7SEG_SEG_A, 50, 51);
-        dMinutesO.mapSegment(_7SEG_SEG_B, 52, 53);
+        dMinutesO.mapSegment(_7SEG_SEG_A, 48, 49);
+        dMinutesO.mapSegment(_7SEG_SEG_B, 46, 47);
         dMinutesO.mapSegment(_7SEG_SEG_C, 56, 57);
-        dMinutesO.mapSegment(_7SEG_SEG_D, 45, 44);
-        dMinutesO.mapSegment(_7SEG_SEG_E, 47, 46);
-        dMinutesO.mapSegment(_7SEG_SEG_F, 49, 48);
-        dMinutesO.mapSegment(_7SEG_SEG_G, 55, 54);
+        dMinutesO.mapSegment(_7SEG_SEG_D, 54, 55);
+        dMinutesO.mapSegment(_7SEG_SEG_E, 52, 53);
+        dMinutesO.mapSegment(_7SEG_SEG_F, 50, 51);
+        dMinutesO.mapSegment(_7SEG_SEG_G, 44, 45);
 
         display.setColor(false, CRGB::Black);
         display.setColor(true, CRGB::Red);
